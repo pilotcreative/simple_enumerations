0f7198078f769b531f18f343403dbdbb3df81805
Mateusz Drożdżyński <matid@matid.net>
2010-03-24 11:31:32 +0100

    Load enumerations after including in ActiveRecord

--- lib/simple_enumerations/simple_enumerations.rb
+++ lib/simple_enumerations/simple_enumerations.rb
@@ -2,48 +2,51 @@ module Wijet
   module SimpleEnumerations
     def self.included(base)
       base.extend ClassMethods
+      EnumerationString::Enumeration.setup
     end
-    
+
     module ClassMethods
       def has_enumerated(name)
         name = name.to_s
-        
+
         define_method("#{name}=") do |value|
           value = value.to_s
           unless Enumeration[name].include?(value) or value.blank?
-            raise ArgumentError, "'#{value}' is not a value of enumeration '#{name}'" 
+            raise ArgumentError, "'#{value}' is not a value of enumeration '#{name}'"
           end
           write_attribute(name, value)
         end
-        
+
         define_method(name) do
           EnumerationString::Enumeration.new(attributes[name])
         end
       end
     end
-    
+
     module EnumerationString
       class Enumeration < String
         def initialize(value = "")
           super(value)
         end
-        
+
         class << self
+          def setup
+            all.values.flatten.uniq.each do |value|
+              define_method("#{value.gsub(/[^[:alnum:]]/, '_')}?") { self == value }
+            end unless all.blank?
+          end
+
           def [](name)
             raise ArgumentError, "Enumeration '#{name}' doesn't exist" unless all.has_key?(name.to_s)
             all[name.to_s]
           end
-      
+
           def all
             Rails.cache.fetch('_simple_enumerations_') do
               YAML::load(File.read(File.join(RAILS_ROOT, 'config', 'enumerations.yml')))
             end
           end
         end
-      
-        all.values.flatten.uniq.each do |value|
-          define_method("#{value.gsub(/[^[:alnum:]]/, '_')}?") { self == value }
-        end unless all.blank?
 
         def ==(object)
           super(object.to_s)
